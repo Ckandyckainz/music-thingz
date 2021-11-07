d7e88fb20ebb7d2b2428e2dad4b1c71b113d94fb
Ckandyckainz <74159422+Ckandyckainz@users.noreply.github.com>
2021-11-07 11:53:09 -0800

    basic setup
    added index.html and code.js

---
+++ code.js
@@ -0,0 +1,6 @@
+let startPlayingButton = document.getElementById("startplayingbutton");
+
+function startPlayingButtonClicked(){
+
+}
+startPlayingButton.addEventListener("click", startPlayingButtonClicked);
